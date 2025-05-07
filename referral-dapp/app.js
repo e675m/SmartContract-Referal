eb1d8a5c1cafe3223b95adced007d4ef63aa67ed
e675m <150522688+e675m@users.noreply.github.com>
2025-05-08 01:55:05 +0300

    Update app.js

--- referral-dapp/app.js
+++ referral-dapp/app.js
@@ -1,5 +1,187 @@
-const contractAddress = "0x1c91347f2A44538ce62453BEBd9Aa907C662b4bD"; // Замените на адрес вашего контракта
+
+const contractAddress = "0xDf73256971cd49f229f24d41b20E5b8339593E1a"; заменить тк последняя версия контракта пока что не была верефицирована 
 const contractABI = [
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "approve",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "allowance",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "needed",
+				"type": "uint256"
+			}
+		],
+		"name": "ERC20InsufficientAllowance",
+		"type": "error"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "sender",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "balance",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "needed",
+				"type": "uint256"
+			}
+		],
+		"name": "ERC20InsufficientBalance",
+		"type": "error"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "approver",
+				"type": "address"
+			}
+		],
+		"name": "ERC20InvalidApprover",
+		"type": "error"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "receiver",
+				"type": "address"
+			}
+		],
+		"name": "ERC20InvalidReceiver",
+		"type": "error"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "sender",
+				"type": "address"
+			}
+		],
+		"name": "ERC20InvalidSender",
+		"type": "error"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			}
+		],
+		"name": "ERC20InvalidSpender",
+		"type": "error"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			}
+		],
+		"name": "OwnableInvalidOwner",
+		"type": "error"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			}
+		],
+		"name": "OwnableUnauthorizedAccount",
+		"type": "error"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Approval",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "previousOwner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "OwnershipTransferred",
+		"type": "event"
+	},
 	{
 		"inputs": [
 			{
@@ -23,7 +205,7 @@ const contractABI = [
 				"type": "address"
 			},
 			{
-				"indexed": false,
+				"indexed": true,
 				"internalType": "address",
 				"name": "referrer",
 				"type": "address"
@@ -31,6 +213,173 @@ const contractABI = [
 		],
 		"name": "Registered",
 		"type": "event"
+	},
+	{
+		"inputs": [],
+		"name": "registerSolo",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "renounceOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "transfer",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Transfer",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "transferFrom",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "transferOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "user",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "newBonus",
+				"type": "uint256"
+			}
+		],
+		"name": "UpdateBonus",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			}
+		],
+		"name": "allowance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			}
+		],
+		"name": "balanceOf",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
 	},
 	{
 		"inputs": [
@@ -46,6 +395,19 @@ const contractABI = [
 				"internalType": "uint256",
 				"name": "",
 				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "decimals",
+		"outputs": [
+			{
+				"internalType": "uint8",
+				"name": "",
+				"type": "uint8"
 			}
 		],
 		"stateMutability": "view",
@@ -72,12 +434,64 @@ const contractABI = [
 	},
 	{
 		"inputs": [],
+		"name": "getLastPrice",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getMyBonus",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
 		"name": "getReferrers",
 		"outputs": [
 			{
 				"internalType": "address[]",
 				"name": "",
 				"type": "address[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "name",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
 			}
 		],
 		"stateMutability": "view",
@@ -125,84 +539,154 @@ const contractABI = [
 		],
 		"stateMutability": "view",
 		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "symbol",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalSupply",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
 	}
 ];
 
-let provider;
-let signer;
-let contract;
-let currentAccount;
+  let provider, signer, contract, account;
 
-async function init() {
-	if (!window.ethereum) {
-	  alert("Установите MetaMask!");
-	  return;
-	}
-  
-	try {
-	  provider = new ethers.providers.Web3Provider(window.ethereum);
-	  await provider.send("eth_requestAccounts", []);
-	  signer = provider.getSigner();
-	  currentAccount = await signer.getAddress();
-  
-	  // Проверяем сеть
-	  const network = await provider.getNetwork();
-	  if (network.chainId !== 11155111) { // ChainID Sepolia
-		alert("Переключитесь на Sepolia в MetaMask!");
-		return;
-	  }
-  
-	  contract = new ethers.Contract(contractAddress, contractABI, signer);
-	  console.log("Контракт инициализирован:", contract);
-  
-	  document.getElementById("account").innerText = `Аккаунт: ${currentAccount}`;
-	  await showBonus();
-	} catch (err) {
-	  console.error("Ошибка инициализации:", err);
-	  alert("Ошибка: " + err.message);
-	}
-  }
+async function connect() {
+  if (window.ethereum) {
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+    provider = new ethers.providers.Web3Provider(window.ethereum);
+    signer = provider.getSigner();
+    account = await signer.getAddress();
+    document.getElementById("userAddress").innerText = account;
 
-async function register() {
-	try {
-	  const referrer = document.getElementById("referrer").value.trim();
-	  console.log("Пробуем зарегистрироваться с реферером:", referrer);
-  
-	  // Если поле реферера пустое, используем нулевой адрес
-	  const referrerAddress = referrer || ethers.constants.AddressZero;
-	  const tx = await contract.register(referrerAddress);
-	  await tx.wait();
-	  
-	  console.log("Регистрация прошла, бонусы обновлены");
-	  alert("Регистрация прошла!");
-	  await showBonus();
-	} catch (err) {
-	  console.error("Ошибка регистрации:", err);
-	  alert("Ошибка регистрации: " + err.message);
-	}
-  }
-  
-  
+    contract = new ethers.Contract(contractAddress, contractABI, signer);
+	
 
-async function getReferrers() {
-  try {
-    const refs = await contract.getReferrers();
-    document.getElementById("referrers").innerText = `Рефералы: ${refs.join(", ")}`;
-  } catch (err) {
-    console.error(err);
+	const ownerAddress = await contract.owner();
+	
+	if(account.toLowerCase() === ownerAddress.toLowerCase()) {
+		const adminHTML = `
+        <div id="adminPanel" class="card p-3 mb-4">
+          <h4>Админ-панель</h4>
+          <button class="btn btn-danger" onclick="someAdminAction()">Действие</button>
+        </div>
+      `;
+      document.getElementById("adminContainer").innerHTML = adminHTML;
+	}
+    
+	const isRegistered = await contract.registered(account);
+    if(!isRegistered){
+      document.getElementById("registrationBlock").classList.remove("d-none");
+    } else {
+      document.getElementById("registrationBlock").classList.add("d-none");
+      getMyBonus();
+      getMyReferrals();
+    }
+  } else {
+    alert("Metamask не найден");
   }
 }
 
-async function showBonus() {
+async function register() {
+  const referrer = document.getElementById("referrerInput").value;
+  try {
+    const tx = await contract.register(referrer);
+    await tx.wait();
+    document.getElementById("registerStatus").innerText = "✅ Регистрация успешна";
+	await connect;
+  } catch (err) {
+    document.getElementById("registerStatus").innerText = "❌ Ошибка: " + err.message;
+  }
+}
+
+async function selfRegister() {
+  try {
+    const tx = await contract.registerSolo();
+    await tx.wait();
+    document.getElementById("registerStatus").innerText = "✅ Регистрация без реферала успешна";
+	await connect;
+  } catch (err) {
+    document.getElementById("registerStatus").innerText = "❌ Ошибка: " + err.message;
+  }
+}
+
+async function checkUserBonus() {
+	const user = document.getElementById("userToCheck").value;
 	try {
-	  const bonus = await contract.getBonus(currentAccount);
-	  console.log("Бонус получен:", bonus);
-	  document.getElementById("bonusDisplay").innerText = `Ваш бонус: ${bonus}`;
-	} catch (err) {
-	  console.error("Ошибка получения бонуса:", err);
-	  document.getElementById("bonusDisplay").innerText = "Ошибка при получении бонуса";
+	  const bonus = await contract.getBonus(user);w
+	  document.getElementById("userBonus").innerText = formatTokens(bonus.toString());
+	} catch (error) {
+		const message = error?.data?.message || error?.message || "";
+		if (message.includes("User is not registered")) {
+		  alert("Пользователь не зарегистрирован");
+		} else {
+		  alert("Ошибка при получении бонуса: " + message);
+		}
 	}
   }
   
 
-window.addEventListener("load", init);
+async function getMyBonus() {
+  try {
+    const bonus = await contract.getMyBonus();
+    document.getElementById("bonusAmount").innerText = formatTokens(bonus.toString());
+  } catch (error) {
+	const message = error?.data?.message || error?.message || "";
+	if (message.includes("User is not registered")) {
+		alert("Требуется регистрация");
+	} else {
+    	alert("Ошибка при получении бонуса: " + message);
+	}
+  }
+}
+
+async function getMyReferrals() {
+  try {
+    const referrals = await contract.getReferrers();
+    const list = document.getElementById("referralList");
+    list.innerHTML = "";
+    referrals.forEach(addr => {
+      const li = document.createElement("li");
+      li.textContent = addr;
+      list.appendChild(li);
+    });
+  } catch (err) {
+    alert("Ошибка при получении рефералов: " + err.message);
+  }
+}
+
+function formatTokens(value, decimals = 18) {
+  return (Number(value) / 10 ** decimals).toFixed(4);
+}
+
+async function showEthPrice() {
+  try {
+    const price = await contract.getLastPrice(); // возвращает int, но в wei (8 знаков)
+    const ethUsd = Number(price.toString()) / 10 ** 8;
+    document.getElementById("ethPrice").innerText = ethUsd.toFixed(2);
+
+    const reward = ethUsd / 10; // 1/10 от курса
+    document.getElementById("expectedBonus").innerText = reward.toFixed(2);
+  } catch (err) {
+    alert("Ошибка при получении курса ETH: " + err.message);
+  }
+}

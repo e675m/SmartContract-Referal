b403c6a5e741a5165f8067c8dcdf24f1cd05e0c6
e675m <150522688+e675m@users.noreply.github.com>
2025-05-29 08:12:17 +0300

    Update RefferalSystem.sol
    Полное изменение логики

--- RefferalSystem.sol
+++ RefferalSystem.sol
@@ -1,77 +1,66 @@
-    // SPDX-License-Identifier: MIT
-    pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-    import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-    import "@openzeppelin/contracts/access/Ownable.sol";
-    import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
+contract ReferralProgram is ERC20, Ownable {
+    mapping(address => bool) public registered;
+    mapping(address => uint256) public referralActions;
 
-    contract ReferralProgram is ERC20, Ownable {
-        
-        mapping(address => address[]) public referrers;
-        mapping(address => uint256) public bonuses;
-        mapping(address => bool) public registered;
+    AggregatorV3Interface internal priceFeed;
+    address public oracle;
 
-        AggregatorV3Interface internal priceFeed;
-
-        constructor() ERC20("ReferralToken", "RFT") Ownable(msg.sender) {
-            _mint(msg.sender, 70000 * (10 ** decimals())); 
-            priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-            registered[msg.sender] = true;
-            emit Registered(msg.sender, address(0)); 
-        }
-
-        event Registered(address indexed user, address indexed referrer);
-        event UpdateBonus(address indexed user, uint256 newBonus);
-
-        function registerSolo() public {
-            require(!registered[msg.sender], "Already registered");
-            registered[msg.sender] = true;
-            emit Registered(msg.sender, address(0));
-        }
-
-        function register(address referrer) public {
-            require(!registered[msg.sender], "Already registered");
-            require(msg.sender != referrer, "Can't refer yourself");
-            require(referrer != address(0), "Use registerSolo for solo registration!");
-
-            registered[msg.sender] = true;
-
-            uint256 price = uint256(getLastPrice());
-            uint256 reward = uint256(price * (10 ** decimals())) / 10**8 / 10;
-
-        referrers[referrer].push(msg.sender);
-        bonuses[referrer] += reward * 70 / 100;
-        _transfer(owner(), referrer, reward);
-        emit Registered(msg.sender, referrer);
-
-        bonuses[msg.sender] += reward * 30 / 100;
-        _transfer(owner(), msg.sender, reward);
-        emit UpdateBonus(msg.sender, bonuses[msg.sender]);
-        }
-
-        function getLastPrice() public view returns (uint256) {
-            (, int256 price , , ,) = priceFeed.latestRoundData();
-            require(price > 0, "Invalid price");
-            return uint256(price);
-        }
-
-        function getReferrers() public view returns (address[] memory) {
-            return referrers[msg.sender];
-        }
-
-        function getMyBonus() public view returns (uint256) {
-            require(registered[msg.sender], "User is not registered");
-            return bonuses[msg.sender];
-        }
-
-        function getBonus(address user) public view onlyOwner returns (uint256) {
-            require(registered[user], "User is not registered"); 
-            return bonuses[user];
-        }
-
-        function getRef(address user) public view onlyOwner returns (address[] memory) {
-            require(registered[user], "User is not registered");
-            return referrers[user];
-        }
+    constructor() ERC20("ReferralToken", "RFT") Ownable(msg.sender) {
+        _mint(msg.sender, 70000 * 10 ** decimals());
+        priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        registered[msg.sender] = true;
+        oracle = msg.sender;
+        emit Registered(msg.sender);
     }
+
+    modifier onlyOracle() {
+        require(msg.sender == oracle, "Caller is not the oracle");
+        _;
+    }
+
+    event Registered(address indexed user);
+    event ReferralAction(address indexed user, uint256 totalActions);
+
+    function registerSolo() public {
+        require(!registered[msg.sender], "Already registered");
+        registered[msg.sender] = true;
+        emit Registered(msg.sender);
+    }
+
+    function awardBonus(address to) public onlyOracle {
+        require(registered[to], "User not registered");
+
+        uint256 ethPrice = getLastPrice();
+        uint256 bonusUSD = ethPrice / 10;
+        uint256 bonusTokens = bonusUSD * 10 ** decimals() / 10 ** 8;
+
+        require(balanceOf(owner()) >= bonusTokens, "Not enough tokens in the contract");
+
+        _transfer(owner(), to, bonusTokens);
+
+        referralActions[to] += 1;
+        emit ReferralAction(to, referralActions[to]);
+    }
+
+    function getLastPrice() public view returns (uint256) {
+        (, int256 price,,,) = priceFeed.latestRoundData();
+        require(price > 0, "Invalid price");
+        return uint256(price);
+    }
+
+    function setOracle(address _oracle) public onlyOwner {
+        require(_oracle != address(0), "Invalid oracle address");
+        oracle = _oracle;
+    }
+
+    function getReferralActions(address user) public view returns (uint256) {
+        return referralActions[user];
+    }
+}
